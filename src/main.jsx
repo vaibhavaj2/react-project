2ec7798fe47b3c6ac8f0f5cb78bf8a37e1a45cb9
Vaibhav Ajmera <vaibhav.ajmera@spark.co.nz>
2017-08-16 15:37:31 +1200

    Completed the first project.

---
+++ src/main.jsx
@@ -0,0 +1,6 @@
+var React = require('react');
+var ReactDom = require('react-dom');
+
+var ListA = require('./components/List.jsx');
+
+ReactDom.render(<ListA />, document.getElementById('ingredients'));
